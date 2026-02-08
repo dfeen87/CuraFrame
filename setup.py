5b03bf4ffc2e9d7566779a57e0f168accc5cdcda
Don Michael Feeney Jr. <dfeen87@gmail.com>
2026-02-08 15:26:50 -0500

    Install pytest in CI

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="CuraFrame",
+    version="0.1.0",
+    description="Constraint-driven therapeutic design reasoning framework.",
+    packages=find_packages(exclude=("tests", "docs", "apps")),
+    python_requires=">=3.9",
+)
